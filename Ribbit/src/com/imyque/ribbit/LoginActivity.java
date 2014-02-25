05e68bd8e4029efa2e5c6d5d4b8d1b9480068264
Michael Ikhane <michael.ikhane@gmail.com>
2014-02-25 11:06:48 +0100

    Login and Signup atvy completeted

---
+++ Ribbit/src/com/imyque/ribbit/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.imyque.ribbit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity {
+
+	protected TextView mTxvSignUp;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		mTxvSignUp = (TextView) findViewById(R.id.txvSignup);
+		mTxvSignUp.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Intent intent = new Intent(LoginActivity.this, SignupActivity.class);  //Context is the login activity
+				startActivity(intent);
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+}
